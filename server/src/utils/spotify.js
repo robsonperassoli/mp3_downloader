faea78ed418406a76b8f6a5e846db9aa6ee6b870
Robson Gian Perassoli <robsonperassoli@gmail.com>
2018-10-16 11:49:11 -0300

    add refresh token mechanism

--- server/src/utils/spotify.js
+++ server/src/utils/spotify.js
@@ -1,30 +1,37 @@
 import SpotifyWebApi from 'spotify-web-api-node'
 import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from '../config'
+import addSeconds from 'date-fns/add_seconds'
+import isAfter from 'date-fns/is_after'
 
 const spotifyApi = new SpotifyWebApi({
   clientId : SPOTIFY_CLIENT_ID,
   clientSecret : SPOTIFY_CLIENT_SECRET
 })
 
-let accessTokenSet = false
+let tokenExpirationDate = null
 
-async function setAccessToken() {
-  // create logic to validate token expiration time
-  if (!accessTokenSet) {
-      const grantResponse = await spotifyApi.clientCredentialsGrant()
-      spotifyApi.setAccessToken(grantResponse.body.access_token)
-      accessTokenSet = true
+async function grantPermissions() {
+  const tokenSet = !!tokenExpirationDate
+  const tokenExpired = tokenSet ? isAfter(new Date(), tokenExpirationDate) : false
+
+  if (!tokenSet || tokenExpired) {
+    console.log('requesting a new access token from spotify')
+
+    const grantResponse = await spotifyApi.clientCredentialsGrant()
+    const { access_token: accessToken, expires_in: expiresIn } = grantResponse.body
+    spotifyApi.setAccessToken(accessToken)
+    tokenExpirationDate = addSeconds(new Date(), expiresIn - 60)
   }
 }
 
 export async function search (query) {
-  await setAccessToken()
+  await grantPermissions()
   return spotifyApi.search(query, ['playlist'], { limit: 20, offset: 0 })
     .then(data => data.body.playlists)
 }
 
 export async function getPlaylist (userId, playlistId) {
-  await setAccessToken()
+  await grantPermissions()
   const playlistPromise = spotifyApi.getPlaylist(userId, playlistId)
   const tracksPromise = spotifyApi.getPlaylistTracks(userId, playlistId)
 
